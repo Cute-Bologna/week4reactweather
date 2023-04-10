5397a1ba195808c216ccd9a78965b9909b7c0d54
Cute-Bologna <96445537+Cute-Bologna@users.noreply.github.com>
2023-04-10 18:36:06 -0400

    transplanted react from codesandbox

---
+++ src/Weather.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      description: response.data.weather[0].description,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "5ef4de8cd6b7fefcd7c42f98cf464ce8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city . . ."
+        onChange={updateCity}
+      />
+      <button type="Submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temp: {Math.round(weather.temperature)}C&deg</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}kpr</li>
+          <img src={weather.icon} alt="Weahter Icon" />
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
